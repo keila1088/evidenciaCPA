a8f3bbee5c86140a1c8c1373d4f5485177521d87
user <user@eze1-lmc-b02825.local>
2022-02-09 21:27:01 -0300

    items 1 y 2 de ejercicio

---
+++ basic funcional js/ejercicio1/data.js
@@ -0,0 +1,64 @@
+const users = [
+    {
+     "name": "Emanuel Heidenreich",
+     "gender": "Female",
+     "id": "1",
+     "age": 80
+    },
+    {
+     "name": "Noel Nienow",
+     "gender": "Cis Female",
+     "id": "2",
+     "age": 10
+    },
+    {
+     "name": "Tami Bartell",
+     "gender": "Female",
+     "id": "3",
+     "age": 30
+    },
+    {
+     "name": "Carroll Wiza",
+     "gender": "Trans*Woman",
+     "id": "4",
+     "age": 36
+    },
+    {
+     "name": "Curtis Weber",
+     "gender": "Trans Man",
+     "id": "5",
+     "age": 11
+    },
+    {
+     "name": "Nancy Harvey",
+     "gender": "Female",
+     "id": "6",
+     "age": 17
+    },
+    {
+     "name": "Tanya Hamill",
+     "gender": "Female",
+     "id": "7",
+     "age": 30
+    },
+    {
+     "name": "Bruce Rippin",
+     "gender": "Man",
+     "id": "8",
+     "age": 23
+    },
+    {
+     "name": "Hubert Hessel I",
+     "gender": "Androgyne",
+     "id": "9",
+     "age": 15
+    },
+    {
+     "name": "Kelvin Lindgren",
+     "gender": "Hermaphrodite",
+     "id": "10",
+     "age": 66
+    }
+   ];
+
+module.exports.users = users;
